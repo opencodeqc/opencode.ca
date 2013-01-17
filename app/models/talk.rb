0987bc053ccee11724ea37e6a64123b2bff2c199
Rémi Prévost <remi@exomel.com>
2013-01-17 08:37:13 -0500

    OpenCode #9!

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,4 +1,6 @@
 class Talk < YamlRecord::Base
+  include Sprockets::Helpers # OMG, this is unnacceptable
+
   # Source
   source File.expand_path('../../../data/talks',  __FILE__)
 
@@ -7,10 +9,18 @@ class Talk < YamlRecord::Base
 
   # Custom methods
   def author_picture_url
-    self.attributes[:author_picture_url] ||= "http://twitter.com/api/users/profile_image/#{self.author_screenname}"
+    self.attributes[:author_picture_url] || begin
+      if self.author_screenname
+        "http://twitter.com/api/users/profile_image/#{self.author_screenname}"
+      else
+        image_path("default-avatar.png")
+      end
+    end
   end
 
   def author_url
-    self.attributes[:author_url] ||= "http://twitter.com/#{self.author_screenname}"
+    self.attributes[:author_url] || begin
+      "http://twitter.com/#{self.author_screenname}" if self.author_screenname
+    end
   end
 end
